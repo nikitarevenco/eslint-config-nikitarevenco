bb5c29aac7ae6c3bb34de7e64eda7c1d6ee7e605
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-19 11:54:34 +0100

    feat: create basic rule export

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-export function add(x: number, y: number): number {
-  return x + y;
-}
+const rule = [
+  "error",
+  {
+    argsIgnorePattern: "^_$",
+    varsIgnorePattern: "^_$",
+    caughtErrorsIgnorePattern: "^_$",
+  },
+] as any;
+
+module.exports = rule;
