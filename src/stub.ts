fa61ba5d6ae49e9b02304832d1db0c4b51dbe0f8
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-04 20:42:55 +0100

    chore: use ts-disable instead of declare module

--- src/stub.ts
+++ src/stub.ts
@@ -13,8 +13,7 @@ import _import from "eslint-plugin-import";
 import _jsxA11y from "eslint-plugin-jsx-a11y";
 // @ts-expect-error -- No declaration for this module
 import _promise from "eslint-plugin-promise";
-declare module "eslint-plugin-react";
-
+// @ts-disable
 import _react from "eslint-plugin-react";
 // @ts-expect-error -- No declaration for this module
 import _reactHooks from "eslint-plugin-react-hooks";
